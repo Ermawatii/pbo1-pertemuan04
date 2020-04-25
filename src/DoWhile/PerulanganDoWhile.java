82e9b590f56dbc60a3af383273200eb4fcd45a64
Ermawatii <62318461+Ermawatii@users.noreply.github.com>
2020-04-25 09:58:49 +0700

    Contoh Program Perulangan Do While

---
+++ src/DoWhile/PerulanganDoWhile.java
@@ -0,0 +1,19 @@
+package DoWhile;
+
+public class PerulanganDoWhile {
+    public static void main(String[] args) {
+        int IsiGelas = 0;
+        int penuh = 225;
+
+        System.out.println("Isi Gelas Sekarang : " + IsiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+        do {
+            IsiGelas++;
+            System.out.println("sedang mengisi gelas..");
+            System.out.println("Isi Gelas Sekarang : " + IsiGelas + "ml");
+        }while(IsiGelas != penuh);
+
+        System.out.println("Finale : Isi Gelas Sekarang : "+ IsiGelas + "ml");
+    }
+}
