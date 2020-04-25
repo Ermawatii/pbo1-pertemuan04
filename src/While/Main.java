19a475be425d2c7b9623caddc816f746ead7cd1b
Ermawatii <62318461+Ermawatii@users.noreply.github.com>
2020-04-25 09:50:00 +0700

    Contoh Program Perulangan While

---
+++ src/While/Main.java
@@ -0,0 +1,19 @@
+package While;
+
+public class Main {
+    public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
+
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+        while(isiGelas != penuh){
+            isiGelas++;
+            System.out.println("sedang mengisi gelas..");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+
+        }
+        System.out.println("finale : Isi Gelas Sekarang : "+ isiGelas + "ml");
+    }
+}
